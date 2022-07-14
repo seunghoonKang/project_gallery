9218fb9828b49114af04cd553bed9ea8c061cae2
SeWoong <pso6222@gmail.com>
2022-07-14 15:23:42 +0900

    팀원 모집 서비스, 라우터 작성

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import express from 'express';
-import { userRouter } from './routers';
+import { userRouter, projectProposalBoardRouter } from './routers';
 import { errorHandler } from './middlewares';
 
 const app = express();
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 // 아래처럼 하면, userRouter 에서 '/login' 으로 만든 것이 실제로는 앞에 /api가 붙어서
 // /api/login 으로 요청을 해야 하게 됨. 백엔드용 라우팅을 구분하기 위함임.
 app.use('/api/user', userRouter);
+app.use('/api/Proposal', projectProposalBoardRouter);
 
 app.use(errorHandler);
 
--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,1 +1,3 @@
 export * from './user-service';
+export * from './projectProposalBoard-service';
+export * from './teamRecruitmentBoard-service';
