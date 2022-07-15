bca7aff15f07188dd8dd0fe53ae75b00b69dcc2f
soheee <soan32@hanmail.net>
2022-07-15 18:46:45 +0900

    style-front-displaytap

---
+++ client/src/display/displayProject.js
@@ -0,0 +1,13 @@
+import { ProjectDetail } from '../share/projectDetail';
+import DisplayTap from './displayTap';
+
+function DisplayProject() {
+  return (
+    <>
+      <ProjectDetail />
+      <DisplayTap />
+    </>
+  );
+}
+
+export { DisplayProject };
