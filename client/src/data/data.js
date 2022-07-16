d50d0e021387a33669ff86731f7c327e141d587f
soheee <soan32@hanmail.net>
2022-07-16 18:57:46 +0900

    featur-front-exhibitionDetail

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,26 +1,40 @@
 const project = [
   {
-    tags: ['C', 'C++', 'JAVA'],
-    image:
+    _id: '11111',
+    nickname: '닉네임이다',
+    time: '7월3일',
+    tags: ['C', 'C++', 'javascript'],
+    image: [
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
+    ],
     title: '프로젝트 제목1',
+    url: 'url임',
     contents: '프로젝트 내용은 .?....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
+    nickname: '닉네임이다',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
     title: '프로젝트 제목2',
+    url: 'url임',
     contents: '프로젝트 내용은 .....이런저런.??...',
   },
   {
+    _id: '33333',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
     title: '프로젝트 제목3',
+    url: 'url임',
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
@@ -28,6 +42,8 @@ const project = [
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
@@ -35,6 +51,8 @@ const project = [
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
@@ -42,6 +60,7 @@ const project = [
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
@@ -49,6 +68,8 @@ const project = [
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
@@ -56,6 +77,8 @@ const project = [
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
@@ -63,6 +86,8 @@ const project = [
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
@@ -70,6 +95,8 @@ const project = [
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
@@ -77,6 +104,8 @@ const project = [
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
@@ -84,6 +113,8 @@ const project = [
     contents: '프로젝트 내용은 .....이런저런....',
   },
   {
+    _id: '2222',
+    time: '7월3일',
     tags: ['C', 'C++', 'JAVA'],
     image:
       'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
