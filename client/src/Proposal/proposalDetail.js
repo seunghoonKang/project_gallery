b270651fb04ce6b494034c74ae74e1919a0d87e1
tmdgns3319 <tmdgns4321@gmail.com>
2022-07-21 01:15:04 +0900

    bugFix-front-수정

--- client/src/Proposal/proposalDetail.js
+++ client/src/Proposal/proposalDetail.js
@@ -1,18 +1,18 @@
 import { useParams } from 'react-router-dom';
 import { ProjectDetail } from '../share/projectDetail';
 import { useEffect, useState } from 'react';
-import { exhibition } from '../api/exhibition/exhibitionProject';
 import axios from 'axios';
 
 function PropsalDetail() {
+  const [proposalContent, setProposalContent] = useState({});
   const { id } = useParams();
   //const [project, setProject] = useState([]);
 
-  // useEffect(() => {
-  //   axios
-  //     .get(`http://localhost:8000/api/exhibition/post/${id}`)
-  //     .then((res) => console.log(res));
-  // }, []);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/exhibition/postId/${id}`)
+      .then((res) => setProposalContent(res.data));
+  }, []);
 
   return (
     <>
