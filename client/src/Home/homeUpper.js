8ee94d97df4a99e433744b94d12916ff414e3efd
tmdgns3319 <tmdgns4321@gmail.com>
2022-07-23 00:37:15 +0900

    css수정

--- client/src/Home/homeUpper.js
+++ client/src/Home/homeUpper.js
@@ -1,17 +1,13 @@
 import styled from 'styled-components';
 
 const HomeUpper = () => {
-  return (
-    <UpperPart>
-      <h1>Welcome our Gallery</h1>
-    </UpperPart>
-  );
+  return <UpperPart></UpperPart>;
 };
 
 const UpperPart = styled.div`
   width: 100vw;
   height: 100vh;
-  background: url(https://cdn.pixabay.com/photo/2016/12/15/07/54/horseshoe-bend-1908283_1280.jpg);
+  background: url(https://elice-pg-s3-bucket.s3.ap-northeast-2.amazonaws.com/eric-park-QbX8A8eHfzw-unsplash.jpg);
   background-size: cover;
 
   & h1 {
