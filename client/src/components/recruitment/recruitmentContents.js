f89f64b878ea9d2e6570356158996870a707e45d
soheee <soan32@hanmail.net>
2022-07-29 23:28:49 +0900

    버튼생성

--- client/src/components/recruitment/recruitmentContents.js
+++ client/src/components/recruitment/recruitmentContents.js
@@ -1,14 +1,25 @@
 import styled from 'styled-components';
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 //url공유하는 코드
 
 function RecruitmentContents({ exhibitionProject }) {
+  const navigate = useNavigate();
+  console.log(exhibitionProject);
+
   return (
     <>
       <div style={{ color: 'white' }}>
         <Container>
           <Nick>{exhibitionProject.nickName}</Nick>
+          <Button
+            onClick={() => {
+              navigate('/messenger');
+            }}
+          >
+            Messenger
+          </Button>
           <Hr />
           <h2 style={{ display: 'inline-block' }}>{exhibitionProject.title}</h2>
 
@@ -27,6 +38,18 @@ function RecruitmentContents({ exhibitionProject }) {
     </>
   );
 }
+
+export default RecruitmentContents;
+const Button = styled.button`
+  border-radius: 5px;
+  width: 90px;
+  height: 40px;
+  color: white;
+  background-color: rgb(42, 53, 200);
+  border: none;
+  
+  }
+`;
 const Nick = styled.h5`
   color: white;
 `;
@@ -46,5 +69,3 @@ const ContentsContainer = styled.div`
   min-height: 200px;
   border-width: 1px;
 `;
-
-export default RecruitmentContents;
